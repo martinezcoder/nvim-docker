d27962f0bfa615cbe64baa9ff02c26b5da798439
Fran Martinez <martinezcoder@gmail.com>
2025-07-18 13:34:31 +0200

    Add split to run tests

--- nvim_config/lua/plugins/neotest.lua
+++ nvim_config/lua/plugins/neotest.lua
@@ -14,4 +14,25 @@ return function()
   map('n', '<Leader>to', ':lua require("neotest").output.open()<CR>', options)
   map('n', '<Leader>ta', ':lua require("neotest").run.attach()<CR>', options)
   map('n', '<Leader>ts', ':lua require("neotest").summary.open()<CR>', options)
+
+  -- Run current test in terminal split. This is useful when we have "binding.pry" in the test
+  vim.keymap.set("n", "<Leader>tp", function()
+    local filename = vim.fn.expand("%")
+    local line = vim.fn.line(".")
+    local cmd = string.format("bundle exec rspec %s:%d", filename, line)
+
+    vim.cmd("botright 15split")
+    vim.cmd("terminal " .. cmd)
+    vim.cmd("startinsert")
+  end, { desc = "Run current test in terminal split" })
+
+  -- Run ALL file tests in terminal split
+  vim.keymap.set("n", "<Leader>tP", function()
+    local filename = vim.fn.expand("%")
+    local cmd = string.format("bundle exec rspec %s", filename)
+
+    vim.cmd("botright 15split")
+    vim.cmd("terminal " .. cmd)
+    vim.cmd("startinsert")
+  end, { desc = "Run current test in terminal split" })
 end
